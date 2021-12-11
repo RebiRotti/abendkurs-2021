108f4baddbc9ec5216eef742ccd650fb622119cd
Rebecca <rebecca@creative-web.work>
2021-12-11 11:42:00 +0100

    Update

--- 2021-12-11/phpBSP/edit.php
+++ 2021-12-11/phpBSP/edit.php
@@ -1,6 +1,3 @@
-
-
-
 <!DOCTYPE html>
 <html>
 <head>
@@ -9,26 +6,26 @@
     <?php include 'db.php'; ?>
 </head>
 <body>
-<?php
-$sql = "SELECT * FROM beispiel_1 WHERE id=?"; // SQL with parameters
-$stmt = $conn->prepare($sql);
-$id = (int)$_GET['entry'];
-$stmt->bind_param("i", $id);
-$stmt->execute();
-$result = $stmt->get_result(); // get the mysqli result
-while($row = $result->fetch_assoc()) {
-?>
-<form action="save.php" method="POST">
-    <input type="hidden" name="newEntry" value="2">
-    <input type="hidden" name="editID" value="<?= $row["id"] ?>">
-    <input type="text" name="editName" value="<?= $row["name"] ?>">
-    <button type="submit">Save</button>
-</form>
+    <?php
+    $sql = "SELECT * FROM beispiel_1 WHERE id = ?"; // SQL with parameters
+    $stmt = $conn->prepare($sql);
+    $id = (int)$_GET['entry'];
+    $stmt->bind_param("i", $id);
+    $stmt->execute();
+    $result = $stmt->get_result(); // get the mysqli result
+    while($row = $result->fetch_assoc()) {
+    ?>
+    <form action="save.php" method="POST">
+        <input type="hidden" name="newEntry" value="2">
+        <input type="hidden" name="editID" value="<?= $row["id"] ?>">
+        <input type="text" name="editName" value="<?= $row["name"] ?>">
+        <button type="submit">Save</button>
+    </form>
 
-<?php
+    <?php
 
-}
-$conn->close();
-?>
+    }
+    $conn->close();
+    ?>
 </body>
 </html>
